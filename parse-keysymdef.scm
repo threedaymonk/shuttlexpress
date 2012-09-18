685ed34dee55c07b7933c6fe66326cbdd83f6805
Paul Battley <pbattley@gmail.com>
2012-09-19 00:25:26 +0100

    Tidy up

--- parse-keysymdef.scm
+++ parse-keysymdef.scm
@@ -1,23 +1,21 @@
 (use irregex srfi-13 extras)
 
 (define port (open-input-file "/usr/include/X11/keysymdef.h"))
+(define definition-re
+  '(: "#define" (+ whitespace)
+      "XK_" ($ (+ (or alphanumeric "_"))) (+ whitespace)
+      "0x" ($ (+ hex-digit))))
 
 (print "(define keysymdef (alist->hash-table '(")
 
 (let loop ()
   (let* ((line (read-line port))
-         (more (not (eof-object? line)))
-         (m (and more
-                 (irregex-search
-                    '(: "#define" (+ whitespace) "XK_"
-                        ($ (+ (or alphanumeric "_")))
-                        (+ whitespace) "0x"
-                        ($ (+ hex-digit)))
-                    line))))
+         (line (if (eof-object? line) #f line))
+         (m (and line (irregex-search definition-re line))))
     (when m
       (printf "(~S . #x~A)\n"
               (irregex-match-substring m 1)
               (irregex-match-substring m 2)))
-    (if more (loop))))
+    (if line (loop))))
 
 (print ")))")
