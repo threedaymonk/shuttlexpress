f7a3541f3908a7cd6db7196cd3380e9c05b90a65
Paul Battley <pbattley@gmail.com>
2012-09-19 13:13:34 +0100

    Extract display and window acquisition
    They are both useful elsewhere - for getting the current window title,
    for example.

--- fake-keys.scm
+++ fake-keys.scm
@@ -2,70 +2,91 @@
 (include "keysymdef.scm")
 
 (foreign-declare "#include <X11/Xlib.h>")
-(foreign-declare "#include <X11/keysym.h>")
 
-(foreign-declare "
-  void sendKeyEvent(Display *display, Window window, Window root,
-                    Bool press, int keysym, int modifiers
-  ) {
-    XKeyEvent event;
+; Send a simulated key event to the display and window.
+; press is #t for press, #f for release.
+; keysym is a code obtained from keysymdef.h.
+; modifiers is a mask representing the state of the modifier keys.
+(define send-key-event
+  (foreign-lambda* void ((c-pointer disp) (c-pointer window) (bool press)
+                         (int keysym) (int modifiers))
+    "XKeyEvent event;
 
-    event.display     = display;
-    event.window      = window;
-    event.root        = root;
-    event.subwindow   = None;
-    event.time        = CurrentTime;
-    event.x           = 1;
-    event.y           = 1;
-    event.x_root      = 1;
-    event.y_root      = 1;
-    event.same_screen = True;
-    event.keycode     = XKeysymToKeycode(display, keysym);
-    event.state       = modifiers;
-    event.type        = (press) ? KeyPress : KeyRelease;
+     event.display     = disp;
+     event.window      = *(Window *)window;
+     event.root        = XDefaultRootWindow(disp);
+     event.subwindow   = None;
+     event.time        = CurrentTime;
+     event.x           = 1;
+     event.y           = 1;
+     event.x_root      = 1;
+     event.y_root      = 1;
+     event.same_screen = True;
+     event.keycode     = XKeysymToKeycode(disp, keysym);
+     event.state       = modifiers;
+     event.type        = (press) ? KeyPress : KeyRelease;
+ 
+     XSendEvent(event.display, event.window, True, KeyPressMask, (XEvent *)&event);"))
 
-    XSendEvent(event.display, event.window, True, KeyPressMask, (XEvent *)&event);
-  }
-  ")
+; Call f with the XDisplay object representing the default display.
+; The display is freed after use.
+(define (with-x-display f)
+  (define (open)
+    (foreign-value "XOpenDisplay(NULL)" c-pointer))
+  (define close
+    (foreign-lambda* void ((c-pointer d)) "XCloseDisplay(d);"))
+  (let ((x-display (open)))
+    (f x-display)
+    (close x-display)))
 
-(define send-key-events
-  (foreign-lambda* int ((int keysym) (int modifiers)) "
-    Display *display;
-    Window root, focused;
-    int revert;
+; Call f with the currently focused window.
+; disp is the display, obtained via with-x-display.
+; The window is freed after use. (In fact, it's only a pointer to an unsigned
+; long (probably) but as Chicken's FFI doesn't let us use opaque typedefs,
+; it's being passed as a void pointer and freed afterwards.)
+(define (with-focused-window disp f)
+  (define get-focused-window
+    (foreign-lambda* c-pointer ((c-pointer disp)) "
+      Window *focused;
+      int revert;
+      focused = (Window *)malloc(sizeof(Window));
+      XGetInputFocus(disp, focused, &revert);
+      C_return(focused);"))
+  (define clean-up
+    (foreign-lambda* void ((c-pointer focused)) "
+      free(focused);"))
+  (let ((focused-window (get-focused-window disp)))
+    (f focused-window)
+    (clean-up focused-window)))
 
-    display = XOpenDisplay(NULL);
-    if (display == NULL) C_return(-1);
-
-    root = XDefaultRootWindow(display);
-    XGetInputFocus(display, &focused, &revert);
-
-    sendKeyEvent(display, focused, root, True,  keysym, modifiers);
-    sendKeyEvent(display, focused, root, False, keysym, modifiers);
-
-    XCloseDisplay(display);
-    C_return(0);
-    "))
-
-(define xk-modifier-map
-  '(("Shift" .   1)
-    ("Control" . 4)
-    ("Mod1" .    8)
-    ("Mod2" .   16)
-    ("Mod3" .   32)
-    ("Mod4" .   64)
-    ("Mod5" .  128)))
-
+; Return the bit mask for a named modifier
 (define (xk-modifier-mask name)
-  (cdr (assoc name xk-modifier-map)))
+  (define mapping
+    '(("Shift" .   1)
+      ("Control" . 4)
+      ("Mod1" .    8)
+      ("Mod2" .   16)
+      ("Mod3" .   32)
+      ("Mod4" .   64)
+      ("Mod5" .  128)))
+  (cdr (assoc name mapping)))
 
-(define (decode-modifiers modifiers)
+; Convert a list of named modifiers to a bit mask
+(define (list->modifier-mask modifiers)
   (let loop ((state 0) (modifiers modifiers))
     (if (null? modifiers)
       state
       (loop (bitwise-ior state (xk-modifier-mask (car modifiers)))
             (cdr modifiers)))))
 
+; Send a fake keypress (press, then release).
+; name is a symbol from keysymdef.h without the leading XK_.
+; modifiers is a list of "Shift", "Control", and "Mod1" through "Mod5".
+; Case is significant (because "a" is different to "A").
 (define (fake-keypress name . modifiers)
-  (let ((keysym (hash-table-ref keysymdef name)))
-    (send-key-events keysym (decode-modifiers modifiers))))
+  (let ((keysym (hash-table-ref keysymdef name))
+        (modmask (list->modifier-mask modifiers)))
+    (with-x-display (lambda (disp)
+      (with-focused-window disp (lambda (win)
+        (send-key-event disp win #t keysym modmask)
+        (send-key-event disp win #f keysym modmask)))))))
