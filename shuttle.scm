877fa455f30cd29b1eb1827928e84d2dd4c25875
Paul Battley <pbattley@gmail.com>
2012-09-19 02:30:58 +0100

    Test mapping for mplayer

--- shuttle.scm
+++ shuttle.scm
@@ -2,5 +2,14 @@
 (include "interface.scm")
 (include "fake-keys")
 
+(define mapping
+  '((("jog" -1) .           ("Left"))
+    (("jog"  1) .           ("Right"))
+    (("ring-relative" -1) . ("bracketleft"))
+    (("ring-relative"  1) . ("bracketright"))
+    (("button" 3 1) .       ("p"))))
+
 (handle-shuttle-events (lambda (event)
-   (printf "~S~N"  event)))
+  (let ((command (assoc event mapping)))
+    (if command
+      (apply fake-keypress (cdr command))))))
